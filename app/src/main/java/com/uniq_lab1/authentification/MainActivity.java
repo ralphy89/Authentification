139180da6852bd6bff8907455415c2e939102e1b
ralphy89 <ralphdumera00@gmail.com>
2023-05-09 00:46:54 -0400

    initial commit

---
+++ app/src/main/java/com/uniq_lab1/authentification/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uniq_lab1.authentification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authentification);
+    }
+}
